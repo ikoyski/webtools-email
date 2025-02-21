d250a03f319a4740e65eeb6735a97572e59a07d0
CRSanpedro <CRSanpedro@2023RLD0385-LT.rcgitmanila.com>
2025-02-21 02:03:50 -0800

    added discovery client but not yet tested

--- src/main/java/com/ikoyski/webtoolsemail/WebtoolsEmailApplication.java
+++ src/main/java/com/ikoyski/webtoolsemail/WebtoolsEmailApplication.java
@@ -2,8 +2,10 @@ package com.ikoyski.webtoolsemail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WebtoolsEmailApplication {
 
 	public static void main(String[] args) {
