b90ab25c9a48dfc36d8a32350cb66d0b113362fe
ikoyski <christian_sanpedro@yahoo.com>
2025-02-28 17:38:48 +0000

    updated request mappings

--- src/main/java/com/ikoyski/webtools/email/controller/EmailController.java
+++ src/main/java/com/ikoyski/webtools/email/controller/EmailController.java
@@ -3,6 +3,7 @@ package com.ikoyski.webtools.email.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ikoyski.webtools.email.dto.EmailDetails;
@@ -11,12 +12,13 @@ import com.ikoyski.webtools.email.service.EmailService;
 import jakarta.mail.MessagingException;
 
 @RestController
+@RequestMapping("/email")
 public class EmailController {
 
 	@Autowired
 	private EmailService emailService;
 
-	@PostMapping("api/v1/sendMail")
+	@PostMapping("/v1")
 	public String sendMail(@RequestBody EmailDetails details) throws MessagingException {
 		String status = emailService.sendMail(details);
 		return status;
